cc8f38fd0d1649a189d04bbd11655e59a0f9bfe1
AMANSHARMArdk <amansharma.as781485@gmail.com>
2025-03-28 15:36:48 +0530

    new

---
+++ java_01_basic_of_java.java
@@ -0,0 +1,43 @@
+// What is Java?
+// Java is a popular programming language, created in 1995.
+
+// It is owned by Oracle, and more than 3 billion devices run Java.
+
+// It is used for:
+
+// Mobile applications (specially Android apps)
+// Desktop applications
+// Web applications
+// Web servers and application servers
+// Games
+// Database connection
+// And much, much more!
+// Why Use Java?
+// Java works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+// It is one of the most popular programming languages in the world
+// It has a large demand in the current job market
+// It is easy to learn and simple to use
+// It is open-source and free
+// It is secure, fast and powerful
+// It has huge community support (tens of millions of developers)
+// Java is an object oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs
+// As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa
+
+
+// java is a general purpose, high-level static compiled interpreted object oriented programming language
+
+
+// execution flow of java programming
+// 1. write the code
+// 2. compile the code
+// 3. run the code
+// 4. debug the code
+// 5. deploy the code
+// 6. maintain the code
+// 7. update the code
+// 8. delete the code
+
+//  demo.java(source code)--->compile--->demo.class(byte.code)--->interpreter--->binary code or meachine code--->hardware--->output
+
+// object oriented programming it is a way to write programming code in which we can create objects and classes
+
